a28430988361f4f0024bd444d1caebec0902180f
Riley Reid <rreid112@gmail.com>
2015-07-28 18:27:02 -0600

    Completed Menu Skeleton

---
+++ FitnessApp/ProgressPhotosViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ProgressPhotosViewController.swift
+//  FitnessApp
+//
+//  Created by Riley Reid on 2015-07-28.
+//  Copyright (c) 2015 RJReid. All rights reserved.
+//
+
+import Foundation
+
+class ProgressPhotosViewController: UIViewController {
+    
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if self.revealViewController() != nil {
+            menuButton.target = self.revealViewController()
+            menuButton.action = Selector("revealToggle:")
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+}
